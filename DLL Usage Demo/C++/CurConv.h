1a4fcbf37db3b6f3bd05cf6bf60b1b5b53775a9d
Daniel Marschall <info@daniel-marschall.de>
2017-08-26 23:49:13 +0000

    git-svn-id: https://svn.viathinksoft.com/svn/currency_converter/trunk@5 329da09e-376f-4659-9b51-cf1fc02ac161

---
+++ DLL Usage Demo/C++/CurConv.h
@@ -0,0 +1,41 @@
+#ifndef _CURCONV_API_H_
+#define _CURCONV_API_H_
+
+#include <windows.h>
+
+typedef DWORD CURCONV_FLAGS;
+
+const CURCONV_FLAGS CONVERT_DONT_SHOW_ERRORS             = 1;
+const CURCONV_FLAGS CONVERT_FALLBACK_TO_CACHE            = 2;
+const CURCONV_FLAGS CONVERT_USE_SSL                      = 4;
+const CURCONV_FLAGS CONVERT_CONFIRM_WEB_ACCESS           = 8;
+const CURCONV_FLAGS CONVERT_NO_INTERACTIVE_API_KEY_INPUT = 16;
+
+const HRESULT S_VTSCONV_OK              = 0x20000000; // Success, Customer defined, Facility 0, Code 0
+const HRESULT S_VTSCONV_NOTHING         = 0x20000001; // Success, Customer defined, Facility 0, Code 1
+const HRESULT E_VTSCONV_GENERIC_FAILURE = 0xA0000000; // Failure, Customer defined, Facility 0, Code 0
+const HRESULT E_VTSCONV_BAD_ARGS        = 0xA0000001; // Failure, Customer defined, Facility 0, Code 1
+
+#ifdef BUILDING_DLL
+#define CURCONV_API extern "C" __declspec(dllexport) __stdcall
+#else
+#define CURCONV_API extern "C" __declspec(dllimport) __stdcall
+#endif
+
+CURCONV_API HRESULT DeleteAPIKey(BOOL UserMode, BOOL DontShowErrors);
+
+CURCONV_API HRESULT WriteAPIKeyW(LPCWSTR key, BOOL UserMode, BOOL DontShowErrors);
+CURCONV_API HRESULT WriteAPIKeyA(LPCSTR key, BOOL UserMode, BOOL DontShowErrors);
+
+CURCONV_API HRESULT ReadAPIKeyW(LPWSTR key, BOOL DontShowErrors);
+CURCONV_API HRESULT ReadAPIKeyA(LPSTR key, BOOL DontShowErrors);
+
+CURCONV_API float ConvertW(float Value, LPCWSTR CurFrom, LPCWSTR CurTo, int MaxAge, CURCONV_FLAGS Flags, DATE HistoricDate);
+CURCONV_API float ConvertA(float Value, LPCSTR CurFrom, LPCSTR CurTo, int MaxAge, CURCONV_FLAGS Flags, DATE HistoricDate);
+
+CURCONV_API int AcceptedCurrenciesW(LPWSTR WriteTo, int MaxAge, CURCONV_FLAGS Flags, DATE HistoricDate);
+CURCONV_API int AcceptedCurrenciesA(LPSTR WriteTo, int MaxAge, CURCONV_FLAGS Flags, DATE HistoricDate);
+
+CURCONV_API HRESULT DownloadNow(CURCONV_FLAGS Flags, DATE HistoricDate);
+
+#endif
